feac5274634977780b91a3eddb7a5b8ea03692a0
yasirmehboobitsc <yasir.mehboob@usindh.edu.pk>
2020-07-16 13:30:16 +0500

    YASIR 16-07-2020 1:30PM

---
+++ application/controllers/Form.php
@@ -0,0 +1,63 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Form extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->load->model('Administration');
+		$this->load->model('log_model');
+		$this->load->model("Admission_session_model");
+	}
+
+	public function announcement ()
+	{
+		$admission_announcements = $this->Admission_session_model->get_form_admission_session ();
+
+		$data['user'] = '';
+		$data['user'] = '';
+		$data['profile_url'] = '';
+
+		$data['admission_announcement'] = $admission_announcements;
+
+		$this->load->view('include/header',$data);
+//		$this->load->view('include/preloder');
+//		$this->load->view('include/side_bar');
+//		$this->load->view('include/nav',$data);
+		$this->load->view('display_form_announcement',$data);
+//		$this->load->view('include/footer_area');
+		$this->load->view('include/footer');
+	}
+	public function review ()
+	{
+		$this->form_validation->set_rules('ADMISSION_SESSION_ID','form session','required');
+		$this->form_validation->set_rules('CAMPUS_ID','campus','required');
+		if ($this->form_validation->run())
+		{
+			$ADMISSION_SESSION_ID = html_escape(htmlspecialchars($this->input->post('ADMISSION_SESSION_ID')));
+			$CAMPUS_ID 			  = html_escape(htmlspecialchars($this->input->post('CAMPUS_ID')));
+
+			$data['user'] = '';
+			$data['user'] = '';
+			$data['profile_url'] = '';
+
+			$data['student_profile'] = '';
+
+			$this->load->view('include/header',$data);
+//		$this->load->view('include/preloder');
+//		$this->load->view('include/side_bar');
+//		$this->load->view('include/nav',$data);
+			$this->load->view('form_review',$data);
+//		$this->load->view('include/footer_area');
+			$this->load->view('include/footer');
+
+		}else
+		{
+			echo "please try again";
+		}
+
+	}
+
+}
---
+++ application/views/form_review.php
@@ -0,0 +1,48 @@
+	<!-- Static Table Start -->
+	<div class="static-table-area">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<div class="sparkline8-list">
+						<div class="sparkline8-hd">
+							<div class="main-sparkline8-hd">
+								<h1 class="text-center">APPLICATION FORM REVIEW</h1>
+							</div>
+						</div>
+						<div class="sparkline8-graph">
+							<div class="static-table-list table-responsive">
+								<table class="table table-bordered">
+									<tr>
+										<th>Student Name
+											<span class="text-danger" style="font-size: 9pt">As per in your matriculation certificate</span>
+										</th>
+										<td>Yasir Mehboob</td>
+									</tr>
+									<tr>
+										<th>Father's Name</th>
+										<td>Mehboob</td>
+									</tr>
+									<tr>
+										<th>Surname</th>
+										<td>Rind</td>
+									</tr>
+									<tr>
+										<th>CNIC No</th>
+										<td>4120209109363</td>
+									</tr>
+
+									<tr>
+										<th>Mobile No</th>
+										<td>03331233231</td>
+									</tr>
+
+								</table>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</section>
